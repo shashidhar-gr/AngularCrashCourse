df1ac370d3d73ea87700fd460d3d79cca3190801
shashidhar-gr <shashidhargr26@gmail.com>
2017-09-13 21:17:31 +0530

    Attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,15 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ title }}</h2>
         <img [src] = "imageUrl"/>
+        <table>
+            <tr>
+                <td [attr.colspan] = "colSpan"></td>
+            </tr>
+        </table>
     `
 })
 export class CoursesComponent {
-    title = 'List of courses';
     imageUrl = 'http://lorempixel.com/400/200';
+    colSpan = 2;
 }
