01d4f48e2849e030b2b17735834de579c6380041
shashidhar-gr <shashidhargr26@gmail.com>
2017-09-13 08:39:48 +0530

    Section03 changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { CoursesService } from './courses.service';
+import { CoursesComponent } from './courses.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    CoursesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/courses.component.ts
@@ -0,0 +1,21 @@
+import { CoursesService } from './courses.service';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses',
+    template: `
+        <ul>
+            <li *ngFor = "let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
+})
+export class CoursesComponent {
+    title = 'List of courses';
+    courses;
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
+}
