0edfe85e3eea21dca546955b94100a187f79d0fa
shashidhar-gr <shashidhargr26@gmail.com>
2017-09-13 21:51:37 +0530

    Template variable

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,20 +4,14 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <div (click) = "onDivClick()">
-            <button class = "btn btn-primary" (click) = "onSave($event)">Save</button>
-        </div>
+        <input #email (keyup.enter) = "onKeyUp(email.value)"/>
     `
 })
 export class CoursesComponent {
     imageUrl = 'http://lorempixel.com/400/200';
     colSpan = 2;
 
-    onSave($event) {
-        console.log("Button was clicked!! ",$event);
-    }
-
-    onDivClick() {
-        console.log("Div was clicked!!");
+    onKeyUp(email) {
+        console.log("Enter key pressed!!",email);
     }
 }
